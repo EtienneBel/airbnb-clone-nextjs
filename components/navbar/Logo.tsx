3c7b170220a1e97f5d5ba344c2534191c1df0d25
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-06-27 21:18:08 +0000

    added NavSearch, Logo components

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { LuTent } from 'react-icons/lu'
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <LuTent className='w-6 h-6' />
+      </Link>
+    </Button>
   )
 }
 
